6c57f6f4ee98be22cebb5469d663dbe66b05e859
Javi3166 <javi3166@yahoo.com>
2024-10-20 21:43:58 -0400

    More functions added

--- Main.py
+++ Main.py
@@ -1,3 +1,11 @@
 phrase = "Giraffe Academy"
 
 print(phrase.upper().isupper())
+
+print(len(phrase))
+
+print(phrase[3])
+
+print(phrase.index("Acad"))
+
+print(phrase.replace("Giraffe", "Elephant"))
